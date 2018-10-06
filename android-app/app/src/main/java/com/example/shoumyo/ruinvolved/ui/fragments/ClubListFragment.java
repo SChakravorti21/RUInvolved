e2910b880cf8203a09ca0e704e3568f249755128
Shoumyo <shoumyoarchit@gmail.com>
2018-10-06 12:28:23 -0400

    Fixed club search fragment to be centered

--- android-app/app/src/main/java/com/example/shoumyo/ruinvolved/ui/fragments/ClubListFragment.java
+++ android-app/app/src/main/java/com/example/shoumyo/ruinvolved/ui/fragments/ClubListFragment.java
@@ -1,6 +1,17 @@
 package com.example.shoumyo.ruinvolved.ui.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 public class ClubListFragment extends Fragment {
+
+    public ClubListFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
