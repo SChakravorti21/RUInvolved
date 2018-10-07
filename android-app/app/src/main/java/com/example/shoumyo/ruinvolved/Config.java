9ae85adf6705ec53d490b07ecb89a78f0a4bb533
atharvav <atharva.kench@gmail.com>
2018-10-07 01:22:14 -0400

    changed base url to deployed app

--- android-app/app/src/main/java/com/example/shoumyo/ruinvolved/Config.java
+++ android-app/app/src/main/java/com/example/shoumyo/ruinvolved/Config.java
@@ -1,5 +1,5 @@
 package com.example.shoumyo.ruinvolved;
 
 public class Config {
-    public static final String BASE_URL = "http://10.0.2.2:5000/";
+    public static final String BASE_URL = "https://ru-involved-server.herokuapp.com/";
 }
