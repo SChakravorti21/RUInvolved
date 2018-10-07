b96ec275d2871214d2ccf8b8c5b78735a9ab8d23
Shoumyo <shoumyoarchit@gmail.com>
2018-10-06 22:47:04 -0400

    Added API route to get clubs by list of IDs

--- flask-server/RUInvolvedApp/views/clubs.py
+++ flask-server/RUInvolvedApp/views/clubs.py
@@ -14,10 +14,21 @@ def get_club_with_id():
     club.pop('_id', None)
     return json.dumps(club)
 
+
 @mod.route('/clubs/', methods=['GET'])
 def get_clubs_for_categories():
     categories = request.args.get('categories')
+    if categories:
+        return get_clubs_for_categories(categories)
+    else:
+        club_ids = request.args.get('favorite_clubs')
+        club_ids = [int(club_id) for club_id in club_ids[1:-1].split(',')]
+        print(club_ids)
+        clubs = dbclient.clubs.find({ 'Id': { '$in': club_ids }}, { '_id': 0 })
+        return json.dumps([club for club in clubs])
+    
 
+def get_clubs_for_categories(categories):
     # The categories come as a pseudo-array in the form of a string
     # in the following format: [Category Name 1,Category Name 2,...]
     # Remove the array brackets and split by the commas to get each 
