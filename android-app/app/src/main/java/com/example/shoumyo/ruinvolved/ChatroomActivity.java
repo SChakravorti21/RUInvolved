8f19d0903b66fe94d92257f59b7c42995e6a0df8
atharvav <atharva.kench@gmail.com>
2018-10-06 23:33:11 -0400

    added event listener for club listeer

--- android-app/app/src/main/java/com/example/shoumyo/ruinvolved/ChatroomActivity.java
+++ android-app/app/src/main/java/com/example/shoumyo/ruinvolved/ChatroomActivity.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +15,15 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 
 import com.example.shoumyo.ruinvolved.utils.MessagesAdapter;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -67,6 +72,34 @@ public class ChatroomActivity extends AppCompatActivity {
                 input.setText("");
 
                 displayChatMessages(clubId);
+            }
+        });
+
+        reference.child(clubId + "").addChildEventListener(new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                displayChatMessages(clubId);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
