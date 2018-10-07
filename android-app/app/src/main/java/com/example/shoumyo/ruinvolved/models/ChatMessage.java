60ed3c6f660de133ae988628791bd38a2aa7aeeb
atharvav <atharva.kench@gmail.com>
2018-10-07 09:38:18 -0400

    added ripple color to tabs

--- android-app/app/src/main/java/com/example/shoumyo/ruinvolved/models/ChatMessage.java
+++ android-app/app/src/main/java/com/example/shoumyo/ruinvolved/models/ChatMessage.java
@@ -9,6 +9,7 @@ public class ChatMessage {
     private long messageTime;
     private long clubId;
 
+
     public ChatMessage(String messageText, String messageUser, long clubId) {
         this.messageText = messageText;
         this.messageUser = messageUser;
@@ -26,29 +27,11 @@ public class ChatMessage {
     public String getMessageText() {
         return messageText;
     }
-
-    public void setMessageText(String messageText) {
-        this.messageText = messageText;
-    }
-
     public String getMessageUser() {
         return messageUser;
     }
-
-    public void setMessageUser(String messageUser) {
-        this.messageUser = messageUser;
-    }
-
     public long getMessageTime() {
         return messageTime;
-    }
-
-    public void setMessageTime(long messageTime) {
-        this.messageTime = messageTime;
-    }
-
-    public long getClubId() {
-        return clubId;
     }
 }
 
